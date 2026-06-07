2aec63014841b87e48e178b714e82cb3a0ac4259
aamrits <amritanand888@gmail.com>
2026-06-07 12:30:37 +0530

    minor style change

--- components/DesktopScreen.tsx
+++ components/DesktopScreen.tsx
@@ -93,7 +93,7 @@ export default function DesktopScreen() {
                                             {item.title}
                                         </CardTitle>
 
-                                        <CardDescription className="mt-1 text-white/90 min-">
+                                        <CardDescription className="mt-1 text-white/90 h-[40px]">
                                             {item.shortDescription}
                                         </CardDescription>
                                     </CardHeader>
